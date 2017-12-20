2878a70112a5f1f7680d33455185939f57d63d59
Brayan Espinoza <bjespinozah@gmail.com>
2017-12-19 23:18:28 -0500

    Javascript Added leap example

---
+++ javascript/leap/leap.js
@@ -0,0 +1,16 @@
+
+var Year = function (input) {
+  this.value= input;
+};
+
+Year.prototype.isLeap = function () {
+  if(this.value % 400 == 0){
+    return true;
+  }else if (this.value % 4 == 0 && this.value % 100 != 0) {
+    return true
+  }else {
+    return false;
+  }
+};
+
+module.exports = Year;
